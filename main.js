e902d5df8f4123d5ebc8a9e1f697c6897693998e
wellingtonpereiragodoi <@Wtngodoi1981>
2025-02-10 11:06:50 -0300

    commit inicial

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando" />
 const imgReprovado = '<img src= "./imagens/reprovado.png" alt="Emoji decepicionado" />';
 const atividades=[];
 const notas=[];
-const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
-const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat(prompt("digite a nota mínima:"));
 
 let linhas ='';
 
@@ -30,7 +27,7 @@ if (atividades.includes(inputNomeAtividade.value)){
     let linha ='<tr>';
     linha +=`<td>${inputNomeAtividade.value}</td>`;
     linha +=`<td>${inputNotaAtividade.value}</td>`;
-    linha +=`<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    
     linha +='</tr>';
     
     linhas += linha;
@@ -47,19 +44,3 @@ const corpoTabela = document.querySelector('tbody');
 corpoTabela.innerHTML =linhas;
 }
 
-function atualizaMediafinal(){
-    const mediaFinal = calcularMediaFinal();
-
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-}
-function calcularMediaFinal(){
-    
-    let somaDasNotas =0;
-
-for(let i = 0; i < notas.length; i++){
-    somaDasNotas += notas[i];
-}
-
-return somaDasNotas / notas.length;
-}
